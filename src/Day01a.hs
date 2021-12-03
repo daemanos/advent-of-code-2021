4c177ff24e1f1c0064e93f9188ceeac44536a48f
Daman Morris <damanm72@gmail.com>
2021-12-02 21:04:56 -0500

    Add solution for Day01a

---
+++ src/Day01a.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import qualified Data.Text as Text
+
+solve :: [Int] -> Int
+solve [_] = 0
+solve (x:xs) = increases
+  where (_, increases) = foldl f (x, 0) xs
+        f (last, incs) next = (next, if last < next then incs + 1 else incs)
+
+main =
+  do increases <- solve <$> map read <$> lines <$> getContents
+     print increases
